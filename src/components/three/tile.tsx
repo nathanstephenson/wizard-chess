d828162d2de5803e063dac4f47c0246c50a1cbad
nathanstephenson <nathans261@gmail.com>
2025-04-25 18:59:25 +0000

    add models and start adding them to tile component

--- src/components/three/tile.tsx
+++ src/components/three/tile.tsx
@@ -1,6 +1,9 @@
 import { useState } from "react"
 import { capture, defaultPieces, move } from "@/common/advanced-pieces"
 import { useGame } from "../game"
+import { useLoader } from "@react-three/fiber"
+import { OBJLoader } from "three-stdlib"
+import { AdvancedPiece, AdvancedPieceState } from "@/types/piece"
 
 type TileProps = {
     x: number
@@ -32,7 +35,8 @@ export const Tile = ({ x, z }: TileProps) => {
 
     const offset = -game.state.boardSize / 2 + (game.state.boardSize % 2 === 0 ? 0.5 : 0)
 
-    const hasPiece = game.pieces.find(piece => piece.position.x === x && piece.position.z === z) !== undefined
+    const piece = game.pieces.find(piece => piece.position.x === x && piece.position.z === z)
+    const hasPiece = piece !== undefined
 
     const validTake =
         game.tile.selected !== undefined &&
@@ -58,8 +62,37 @@ export const Tile = ({ x, z }: TileProps) => {
 
     return (
         <mesh position={[offset + x, -1, -offset - z]} onPointerOver={onOver} onPointerOut={onOut} onClick={onClick}>
+            {piece !== undefined ? <P state={piece} /> : <></>}
             <boxGeometry args={[1, 1, 1]} />
             <meshStandardMaterial color={getTileColour()} />
         </mesh>
     )
 }
+
+const P = ({ state }: { state: AdvancedPieceState }) => {
+    const gltf = useLoader(OBJLoader, `../../../models/${getPieceName(state.piece)}.obj`)
+    console.log(gltf)
+    if (state === undefined) return null
+    return <mesh position={[state.position.x, 1, state.position.z]} scale={0.03} rotateZ={Math.PI / 2}>
+        <primitive object={gltf} />
+    </mesh>
+}
+
+const getPieceName = (piece?: AdvancedPiece["code"]) => {
+    if (piece === undefined) return ""
+
+    switch(piece) {
+        case "P":
+            return "Pawn"
+        case "N":
+            return "Knight"
+        case "R":
+            return "Rook"
+        case "B":
+            return "Bishop"
+        case "Q":
+            return "Queen"
+        case "K":
+            return "King"
+    }
+} 
